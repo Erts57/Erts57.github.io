59fa567956171df7da7fc901ce87283d0b45d0ea
Erts57 <73663747+Erts57@users.noreply.github.com>
2022-06-30 19:10:04 -0600

    e

--- index.js
+++ index.js
@@ -359,12 +359,12 @@ class Extras {
         }
     }
     //https://stackoverflow.com/questions/2970525/converting-any-string-into-camel-case
-    camelize(str) {
+    /*camelize(str) {
         return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function (match, index) {
             if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
             return index === 0 ? match.toLowerCase() : match.toUpperCase();
         });
-    }
+    }*/
     boolBlock({
         MENU
     }) {
