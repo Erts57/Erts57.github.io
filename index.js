14a0ff533ac49a1ba9229437780608dbec2ee55d
Erts57 <73663747+Erts57@users.noreply.github.com>
2022-07-02 08:49:34 -0600

    Doing thigns

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-let json = getFile("Info57/updates.json")
+const topnav = "Info57/topnav.json";
+const updates = "Info57/updates.json";
 
-json.updates.forEach(addElement);
-
-function addElement(element) {
-    var updates = document.getElementById("updates");
-    var elementThing = document.createElement(element.element);
-    if (element.element == "p") {
-        elementThing.innerHTML = element.innerText;
+function createTopNavElement() {
+    for (let i = 0; i > data.length; i++) {
+        
+        var element = document.createElement("a");
+        
+        element.innerHTML = data[i].header;
+        element.href = data[i].source;
+        
+        if (data[i].active.includes(window.location.href)) {
+            element.className = "active";
+        }
+        
+        document.getElementsByClassName("topnav")[0].appendChild(element);
     }
-    updates.appendChild(elementThing);
 }
 
-async function getFile(src) {
-    let myObject = await fetch(src)
-    let myJson = myObject.json();
-    return myJson;
-}
+
+(async () => {  
+    try {  
+        const res = await fetch("topnav")  
+        const out = await res.json()  
+        data = out; 
+    } catch (err) {  
+        throw err  
+    }
+})()
+let data = data;
+createTopNavElement();
